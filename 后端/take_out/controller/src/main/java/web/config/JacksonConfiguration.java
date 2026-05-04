bc8722133c373f90f060866eeede13946070b3d0
热爱可抵万难 <2426912619@qq.com>
2026-05-04 16:47:15 +0800

    json解析

--- 后端/take_out/controller/src/main/java/web/config/JacksonConfiguration.java
+++ 后端/take_out/controller/src/main/java/web/config/JacksonConfiguration.java
@@ -21,8 +21,6 @@ import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfiguration {
-
-   
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
     public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
     public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
